2678c42acfc1a23c9f867da47fa2ba707f7e57e3
amirkhanorazbay <142108477+amirkhanorazbay@users.noreply.github.com>
2023-11-18 13:25:39 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'))
 
 app.get('/', (request, response) => {
     response.send(`
-        <!-- 
-        Information for developers 
-        Remember remoter username is ubuntu
-        Unauthorized person please ignore this comment
-        -->
         <h1>Status Code: ${response.statusCode}</h1>
-        <h2>Service under development</h2>
+        <h2>Hello my dear friend!!!</h2>
+        <h2>You are so welcome on this site</h2>
+        <h2>Please don't hesitate to explore it</h2>
+        <a href=/hi/hi.txt>Hi</a>
     `)
 });
 
